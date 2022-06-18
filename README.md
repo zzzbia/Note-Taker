7d88a20fd17035e6fd3a7a04aa967d4a9b7e113f
R a b i a <rabia.saeed.1995@gmail.com>
2022-06-17 23:18:36 -0400

    finished routes and updated README

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # Note-Taker
+<img src = "https://img.shields.io/static/v1?label=JavaScript&message=Node.js&color=blueviolet?style=flat-square&logo=appveyor">
+
+
+## ✏️ Description
+Create a Note Taking App! When you have random throughts or idea's, you may store them in this note taker! You can can save and delete the notes you have made. 
+
+## Table Of Contents
+- [Installation](#💾-installation)
+- [Usage](#💡-usage)
+- [Contributing](#👥-contributing)
+- [Questions](#💭-questions)
+- [License](#📚-license)
+
+## 💾 Installation
+First clone the repo, on to your computer:
+
+```
+git clone
+```
+
+Change directories into the cloned repo and run npm install:
+
+```
+ npm install
+```
+
+## 💡 Usage
+While in the root directory, run:
+
+```
+node index.js
+```
+or 
+```
+npm start
+```
+
+
+
+
+## 👥 Contributing
+To contribute to the Note Taker repo, you can:
+ 1. Please first **Fork** the repo on GitHub
+ 2. **Clone** the project on your computer
+ 3. **Commit** the changes you would like to see
+ 4. **Push** to your existing forked project
+ 5. Finally please submit a **Pull request**
+
+
+
+
+## 💭 Questions
+If you have any questions, please make an issue within the repo or visit my GitHub profile at [zzzbia's github](https://github.com/zzzbia)
+
+
+## 📚 License
+Licensed under [ APACHE](https://opensource.org/licenses/Apache-2)
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -26,7 +26,7 @@ api.get("/notes", (req, res) => {
 
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file,
 //and then return the new note to the client.
-//You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+// give each note a unique id when it's saved (look into npm packages that could do this for you).
 api.post("/notes", (req, res) => {
 	// console.log(req);
 	if (!req.body.title || !req.body.text) {
@@ -61,8 +61,26 @@ api.post("/notes", (req, res) => {
 });
 
 // Bonus
-// You haven’t learned how to handle DELETE requests, but this application has that functionality in the front end. As a bonus, see if you can add the DELETE route to the application using the following guideline:
-
-// DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json
+// handle DELETE requests,
+api.delete("/notes/:id", (req, res) => {
+	const id = req.params.id;
+	try {
+		const notes = JSON.parse(
+			fs.readFileSync(path.resolve(__dirname, "../db/db.json"), "utf8")
+		);
+		const newNotes = notes.filter((note) => note.id !== id);
+		fs.writeFileSync(
+			path.resolve(__dirname, "../db/db.json"),
+			JSON.stringify(newNotes),
+			"utf8"
+		);
+		return res.status(200).json(newNotes);
+	} catch (error) {
+		console.log(error);
+		return res.status(500).send({
+			message: "error deleting note",
+		});
+	}
+});
 
 module.exports = api;
